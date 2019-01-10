d6a0962a190a7694bf80808730476e64b734f629
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2019-01-10 00:02:21 +0000

    added lines to project

---
+++ client/src/SevenColumnCalendar/Line.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { startOfWeek, min, max, addDays, endOfWeek, differenceInDays } from "date-fns";
+
+export function SchedualedPayment(schedualedPayment, gridConfig, firstday) {
+    let paymentSplits = [];
+    let workingStartOfTheWeek = startOfWeek(schedualedPayment.startDate);
+    var next_y = gridConfig.squareSize * Math.abs(differenceInDays(firstday, workingStartOfTheWeek)) / 7 + gridConfig.squareSize;
+    do {
+        let x_start = differenceInDays(max(workingStartOfTheWeek, schedualedPayment.startDate), workingStartOfTheWeek) * gridConfig.squareSize + gridConfig.squareSize / 2;
+        let x_end = differenceInDays(min(endOfWeek(workingStartOfTheWeek), schedualedPayment.endDate), workingStartOfTheWeek) * gridConfig.squareSize + gridConfig.squareSize / 2;
+        paymentSplits.push(<line key={next_y} stroke="red" strokeWidth="8" x1={x_start} x2={x_end} y1={next_y} y2={next_y} strokeLinecap="round" />);
+        workingStartOfTheWeek = addDays(workingStartOfTheWeek, 7);
+        next_y = gridConfig.squareSize + next_y;
+    } while (new Date(schedualedPayment.endDate) > workingStartOfTheWeek)
+    return (
+        <g key={schedualedPayment.dueDate}>
+            {paymentSplits}
+        </g>
+    )
+}
--- client/src/SevenColumnCalendar/index.js
+++ client/src/SevenColumnCalendar/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { startOfWeek, endOfWeek, differenceInDays, format } from "date-fns";
 import { WeekDateDisplay } from "./WeekDateDisplay";
 import { Grid } from "./Grid";
+import { SchedualedPayment } from "./Line";
 
 const gridConfig = {
     squareSize: 30,
@@ -13,6 +14,10 @@ export const SevenColumnCalendar = props => {
     const { schedualedPayments } = lease;
     if (schedualedPayments.length === 0) return <></> 
     const heightOfCalendar = Math.abs(differenceInDays(startOfWeek(lease.startDate), endOfWeek(lease.endDate)) / 7 ) * gridConfig.squareSize + gridConfig.headerFont + 7;
+    let schedualedPaymentSVGs = [];
+    schedualedPayments.forEach(sp => {
+        schedualedPaymentSVGs.push(SchedualedPayment(sp, gridConfig, startOfWeek(lease.startDate)));
+    });
 
     return (
         <div className="sevenDayCalendar">
@@ -23,6 +28,7 @@ export const SevenColumnCalendar = props => {
             <svg width={ `${ gridConfig.squareSize * 7 }px` } height={ `${ heightOfCalendar }px` }>
                 <WeekDateDisplay squareSize={gridConfig.squareSize} headerFontSize={gridConfig.headerFont} />
                 <Grid gridConfig={gridConfig} leaseStartDate={lease.startDate} leaseEndDate={lease.endDate} />
+                {schedualedPaymentSVGs}
             </svg>
         </div>
     )
