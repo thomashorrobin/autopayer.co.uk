20300541fa023902071a7fbd5da73888fce00a7e
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2019-01-13 14:09:18 +0000

    small refactor

--- client/src/SevenColumnCalendar/Line.js
+++ client/src/SevenColumnCalendar/Line.js
@@ -4,10 +4,10 @@ import { startOfWeek, min, max, addDays, endOfWeek, differenceInDays } from "dat
 export function SchedualedPayment(schedualedPayment, gridConfig, firstday) {
     let paymentSplits = [];
     let workingStartOfTheWeek = startOfWeek(schedualedPayment.startDate);
-    var next_y = gridConfig.squareSize * Math.abs(differenceInDays(firstday, workingStartOfTheWeek)) / 7 + gridConfig.squareSize -2;
+    var next_y = calcY(firstday, gridConfig, workingStartOfTheWeek);
     do {
-        let x_start = differenceInDays(max(workingStartOfTheWeek, schedualedPayment.startDate), workingStartOfTheWeek) * gridConfig.squareSize + gridConfig.squareSize / 2;
-        let x_end = differenceInDays(min(endOfWeek(workingStartOfTheWeek), schedualedPayment.endDate), workingStartOfTheWeek) * gridConfig.squareSize + gridConfig.squareSize / 2;
+        let x_start = calcX(gridConfig, max(workingStartOfTheWeek, schedualedPayment.startDate));
+        let x_end = calcX(gridConfig, min(endOfWeek(workingStartOfTheWeek), schedualedPayment.endDate));
         paymentSplits.push(<line key={next_y} stroke="red" strokeWidth="8" x1={x_start} x2={x_end} y1={next_y} y2={next_y} strokeLinecap="round" />);
         workingStartOfTheWeek = addDays(workingStartOfTheWeek, 7);
         next_y = gridConfig.squareSize + next_y;
@@ -17,4 +17,12 @@ export function SchedualedPayment(schedualedPayment, gridConfig, firstday) {
             {paymentSplits}
         </g>
     )
+}
+
+const calcY = (startDate, gridConfig, date) => {
+    return gridConfig.squareSize * Math.floor(Math.abs(differenceInDays(startDate, date)) / 7) + gridConfig.squareSize -2;
+}
+
+const calcX = (gridConfig, date) => {
+    return differenceInDays(date, startOfWeek(date)) * gridConfig.squareSize + gridConfig.squareSize / 2;
 }
