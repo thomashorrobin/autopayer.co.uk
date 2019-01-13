c78e9ab630432012fc6c598635a61378efadd6b0
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2019-01-13 15:17:20 +0000

    got lines to start at the edges of the calendar

--- client/src/SevenColumnCalendar/Line.js
+++ client/src/SevenColumnCalendar/Line.js
@@ -6,8 +6,12 @@ export function SchedualedPayment(schedualedPayment, gridConfig, firstday) {
     let workingStartOfTheWeek = startOfWeek(schedualedPayment.startDate);
     var next_y = calcY(firstday, gridConfig, workingStartOfTheWeek);
     do {
-        let x_start = calcX(gridConfig, max(workingStartOfTheWeek, schedualedPayment.startDate));
-        let x_end = calcX(gridConfig, min(endOfWeek(workingStartOfTheWeek), schedualedPayment.endDate));
+        const s = max(workingStartOfTheWeek, schedualedPayment.startDate);
+        let x_start = calcX(gridConfig, s);
+        if (s.getTime() !== new Date(schedualedPayment.startDate).getTime()) x_start = x_start - gridConfig.squareSize / 2;
+        const e = min(endOfWeek(workingStartOfTheWeek), schedualedPayment.endDate);
+        let x_end = calcX(gridConfig, e);
+        if (e.getTime() !== new Date(schedualedPayment.endDate).getTime()) x_end = x_end + gridConfig.squareSize / 2;
         paymentSplits.push(<line key={next_y} stroke="red" strokeWidth="8" x1={x_start} x2={x_end} y1={next_y} y2={next_y} strokeLinecap="round" />);
         workingStartOfTheWeek = addDays(workingStartOfTheWeek, 7);
         next_y = gridConfig.squareSize + next_y;
