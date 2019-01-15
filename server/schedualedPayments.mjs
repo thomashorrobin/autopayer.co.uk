57ae6ae0a1098a338ef1ece506618bc1f01ea59e
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2019-01-15 13:07:50 +0000

    fixed an error

--- server/schedualedPayments.mjs
+++ server/schedualedPayments.mjs
@@ -1,25 +1,27 @@
 import fetch from "node-fetch";
 
 export const generateSchedualledMonthlyPayments = leaseAggreement => {
-        return new Promise((resolve, reject) => {
-            fetch('http://sample-app-schedualed-payments:80/api/schedualedpaymentsmonthly', {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify(leaseAggreement)
-            })
-            .then(response => response.json())
-            .then(payments => resolve(payments))
-            .catch(e => reject(e));
-        });
-    }
+    let payload = Object.assign({}, leaseAggreement, { monthlyAmount: leaseAggreement.amount });
+    return new Promise((resolve, reject) => {
+        fetch('http://sample-app-schedualed-payments:80/api/schedualedpaymentsmonthly', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(payload)
+        })
+        .then(response => response.json())
+        .then(payments => resolve(payments))
+        .catch(e => reject(e));
+    });
+}
     
 
 export const generateSchedualledWeeklyPayments = leaseAggreement => {
+    let payload = Object.assign({}, leaseAggreement, { weeklyAmount: leaseAggreement.amount });
     return new Promise((resolve, reject) => {
         fetch('http://sample-app-schedualed-payments:80/api/schedualedpaymentsweekly', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(leaseAggreement)
+            body: JSON.stringify(payload)
         })
         .then(response => response.json())
         .then(payments => resolve(payments))
